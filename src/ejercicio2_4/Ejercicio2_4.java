b2afaa3f8654e723cefb28d95eac279b153da7bf
IdeaPad <IdeaPad@DESKTOP-KD4B494>
2021-06-27 15:16:22 -0500

    EJERCICIO 2 -EN PROCESO

---
+++ src/ejercicio2_4/Ejercicio2_4.java
@@ -0,0 +1,154 @@
+package ejercicio2_4;
+
+class Nodo {
+
+    private int dato;
+    private Nodo izquierda, derecha;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.izquierda = this.derecha = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+
+    public void imprimirDato() {
+        System.out.println(this.getDato());
+    }
+}
+
+class Arbol {
+
+    private Nodo raiz;
+
+    public Arbol() {
+
+    }
+
+    public boolean existe(int busqueda) {
+        return existe(this.raiz, busqueda);
+    }
+
+    private boolean existe(Nodo n, int busqueda) {
+        if (n == null) {
+            return false;
+        }
+        if (n.getDato() == busqueda) {
+            return true;
+        } else if (busqueda < n.getDato()) {
+            return existe(n.getIzquierda(), busqueda);
+        } else {
+            return existe(n.getDerecha(), busqueda);
+        }
+
+    }
+
+    public void insertar(int dato) {
+        if (this.raiz == null) {
+            this.raiz = new Nodo(dato);
+        } else {
+            this.insertar(this.raiz, dato);
+        }
+    }
+
+    private void insertar(Nodo padre, int dato) {
+        if (dato > padre.getDato()) {
+            if (padre.getDerecha() == null) {
+                padre.setDerecha(new Nodo(dato));
+            } else {
+                this.insertar(padre.getDerecha(), dato);
+            }
+        } else {
+            if (padre.getIzquierda() == null) {
+                padre.setIzquierda(new Nodo(dato));
+            } else {
+                this.insertar(padre.getIzquierda(), dato);
+            }
+        }
+    }
+
+    private void preorden(Nodo n) {
+        if (n != null) {
+            n.imprimirDato();
+            preorden(n.getIzquierda());
+            preorden(n.getDerecha());
+        }
+    }
+
+    private void inorden(Nodo n) {
+        if (n != null) {
+            inorden(n.getIzquierda());
+            n.imprimirDato();
+            inorden(n.getDerecha());
+        }
+    }
+
+    private void postorden(Nodo n) {
+        if (n != null) {
+            postorden(n.getIzquierda());
+            postorden(n.getDerecha());
+            n.imprimirDato();
+        }
+    }
+
+    public void preorden() {
+        this.preorden(this.raiz);
+    }
+
+    public void inorden() {
+        this.inorden(this.raiz);
+    }
+
+    public void postorden() {
+        this.postorden(this.raiz);
+    }
+}
+
+public class Ejercicio2_4 {
+
+    public static void main(String[] args) {
+
+        Arbol arbol = new Arbol();
+        arbol.insertar(8);
+        arbol.insertar(4);
+        arbol.insertar(10);
+        arbol.insertar(12);
+        arbol.insertar(1);
+        arbol.insertar(0);
+        arbol.insertar(2);
+        arbol.insertar(9);
+        arbol.insertar(20);
+        arbol.insertar(30);
+        arbol.insertar(14);
+        arbol.insertar(16);
+        arbol.insertar(15);
+        arbol.insertar(13);
+
+    }
+    public static int cuantaNodos(Arbol arbol, Nodo nodo){
+    
+    
+       
+    
+    return 0;
+    }
+
+}
